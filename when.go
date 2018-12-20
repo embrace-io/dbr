5eb0f27e7f75ed2d373836f56984db36b7550e68
khisakuni <khisakuni@gmail.com>
2018-12-20 10:21:47 -0800

    Add case

--- when.go
+++ when.go
@@ -23,3 +23,27 @@ func When(cond Builder, value interface{}) Builder {
 		return nil
 	})
 }
+
+func Case(values ...interface{}) Builder {
+	return BuildFunc(func(d Dialect, buf Buffer) error {
+		buf.WriteString("case (")
+		l := len(values)
+		for i, value := range values {
+			if l > 1 && i == l-1 {
+				buf.WriteString("else ")
+			}
+			switch v := value.(type) {
+			case Builder:
+				v.Build(d, buf)
+			default:
+				buf.WriteString(placeholder)
+				buf.WriteValue(v)
+			}
+			if i < l-1 {
+				buf.WriteString(" ")
+			}
+		}
+		buf.WriteString(")")
+		return nil
+	})
+}
--- when_test.go
+++ when_test.go
@@ -36,3 +36,43 @@ func TestWhen(t *testing.T) {
 		require.Equal(t, test.value, buf.Value())
 	}
 }
+
+func TestCase(t *testing.T) {
+	for _, test := range []struct {
+		when  Builder
+		query string
+		value []interface{}
+	}{
+		{
+			when:  Case(When(Eq("col", 1), 1)),
+			query: "case (when (`col` = ?) then ?)",
+			value: []interface{}{1, 1},
+		},
+		{
+			when:  Case(When(Eq("col", 1), 2), 3),
+			query: "case (when (`col` = ?) then ? else ?)",
+			value: []interface{}{1, 2, 3},
+		},
+		{
+			when:  Case(When(Eq("a", 1), 2), Gt("b", 3)),
+			query: "case (when (`a` = ?) then ? else `b` > ?)",
+			value: []interface{}{1, 2, 3},
+		},
+		{
+			when:  Case(When(Eq("col", "a"), 1), When(Eq("col", "b"), 2), 3),
+			query: "case (when (`col` = ?) then ? when (`col` = ?) then ? else ?)",
+			value: []interface{}{"a", 1, "b", 2, 3},
+		},
+		{
+			when:  Case(When(Eq("colA", "a"), Lt("colB", 5)), When(Eq("colA", "b"), Lt("colB", 10)), Lt("colB", 15)),
+			query: "case (when (`colA` = ?) then `colB` < ? when (`colA` = ?) then `colB` < ? else `colB` < ?)",
+			value: []interface{}{"a", 5, "b", 10, 15},
+		},
+	} {
+		buf := NewBuffer()
+		err := test.when.Build(dialect.Clickhouse, buf)
+		require.NoError(t, err)
+		require.Equal(t, test.query, buf.String())
+		require.Equal(t, test.value, buf.Value())
+	}
+}
